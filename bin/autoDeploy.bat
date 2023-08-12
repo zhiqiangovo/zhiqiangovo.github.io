d8b08bb7b3d26678ea98919fb8938b4b28d80012
shizhiqiang <zhiqiangshi_cn@163.com>
2023-08-13 00:34:26 +0800

    feat:update

--- bin/autoDeploy.bat
+++ bin/autoDeploy.bat
@@ -1,8 +1,8 @@
-call npm docs:build
+call npm run docs:build
 cd docs/.vitepress/dist
 
 git init
 git add -A
 git commit -m "auto construct blog"
 
-git push -f git@github.com:zhiqiangovo/zhiqiangovo.github.io.git main:gh-pages
+git push -f git@github.com:zhiqiangovo/zhiqiangovo.github.io.git master:gh-pages
